572582aa02ced96f41d9788247a0c05c2b1b2054
Home PC <oegodf@gmail.com>
2017-09-25 12:58:26 +0400

    Encryption

--- client/src/CriminalClient.java
+++ client/src/CriminalClient.java
@@ -1,7 +1,17 @@
+import com.sun.org.apache.xml.internal.serializer.Encodings;
+
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -16,7 +26,6 @@ public class CriminalClient {
     public static void main(String[] args) {
         String ip = "127.0.0.1";
         int port = 8080;
-        System.out.println("Connecting to server...");
         if (args.length > 0) {
             ip = args[0];
             if (args.length > 1) {
@@ -26,6 +35,24 @@ public class CriminalClient {
                 }
             }
         }
+        System.out.println("WHA");
+        boolean encryption = false;
+        Path filePath = Paths.get("kfile");
+        if (Files.exists(filePath)) {
+            encryption = loadKeys(filePath);
+        } else {
+            try {
+                System.out.println("PRe file");
+                Files.createFile(filePath);
+                encryption = createKeys(filePath);
+            } catch (IOException e) {
+                System.out.println("Unable to load key files! Encryption is disabled");
+            }
+        }
+        if (encryption) {
+            System.out.println("Encryption is enabled");
+        }
+        System.out.println("Connecting to server...");
         try {
             while (!establishConnectionToServer(ip, port)) {
                 TimeUnit.SECONDS.sleep(5);
@@ -36,6 +63,46 @@ public class CriminalClient {
             runMainConnection();
         } catch (Exception ignored) {
 
+        }
+    }
+
+    private static boolean createKeys(Path filePath) {
+        try {
+            System.out.println("Creating file");
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(1024);
+            KeyPair keyPair = keyPairGenerator.genKeyPair();
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
+            byte[] publicKeyBytes = publicKey.getEncoded();
+            byte[] privateKeyBytes = privateKey.getEncoded();
+            Files.write(filePath,publicKeyBytes);
+            Files.write(filePath,privateKeyBytes);
+            System.out.println("cpub " + publicKey.getFormat());
+            System.out.println("cpriv " + privateKey.getFormat());
+            return true;
+        } catch (IOException | NoSuchAlgorithmException e) {
+            return false;
+        }
+    }
+
+    private static boolean loadKeys(Path filePath) {
+        try {
+            byte[] fileArray = Files.readAllBytes(filePath);
+            System.out.println("Len " + fileArray.length);
+            byte[] publicArray = Arrays.copyOfRange(fileArray, 0, 128);
+            byte[] privateArray = Arrays.copyOfRange(fileArray, 128, 256);
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(publicArray);
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateArray);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA", "SUN");
+            PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
+            PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+            System.out.println("pub " + pubKey.getFormat());
+            System.out.println("priv " + privateKey.getFormat());
+            return true;
+        } catch (IOException | NoSuchAlgorithmException | NoSuchProviderException | InvalidKeySpecException e) {
+            System.out.println("Unable to load key files! Encryption is disabled");
+            return false;
         }
     }
 
